3a90dac50874830f042f382c64d48b571d04da92
mzvast <mzvast@gmail.com>
2016-10-08 22:36:47 +0800

    add user model; add ngForm binding to login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../shared/user';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  model = new User('username', 'password');
+
+  constructor() {
+  }
+
+  onSubmit() {
+    console.log("登录中");
+  }
+  get diagnostic() { return JSON.stringify(this.model); }
 
   ngOnInit() {
   }
+
 
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import {routing} from './login.routing';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+/*TODO:为什么FormsModule需要重复导入？*/
     routing
   ],
   declarations: [LoginComponent]
